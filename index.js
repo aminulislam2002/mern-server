c88df0f8cae7ab6547cce143e2f76bdc49811450
aminulislam2002 <aminulislamemon2002@gmail.com>
2024-02-03 20:56:37 +0600

    youtube tags extractor

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const youtubeTags = require("youtube-tags");
+var getYouTubeID = require("get-youtube-id");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,6 +10,21 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+// Post video URL
+app.post("/videoLink", async (req, res) => {
+  //    1st ----- extract the video id form the video link
+  const youtubeUrl = req.body.youtubeUrl;
+  const id = getYouTubeID(youtubeUrl);
+  //    console.log(id);
+
+  //    2nd ----- get the tags of the youtube video
+  const tags = await youtubeTags.getYoutubeTags(id);
+  //   console.log(tags);
+
+  //    3rd ----- send the tags to client site
+  res.send(tags);
 });
 
 app.listen(port, () => {
