bf6a89ad28511a93ea3cc7ce443968920e99f2ee
aminulislam2002 <aminulislamemon2002@gmail.com>
2024-03-04 03:41:50 +0600

    youtube video thumbnail download

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const youtubeTags = require("youtube-tags");
 var getYouTubeID = require("get-youtube-id");
 var youtubeThumbnail = require("youtube-thumbnail");
 var getYoutubeTitle = require("get-youtube-title");
+let downloadImage = require("download-image");
+const download = require("image-downloader");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -36,6 +38,23 @@ app.post("/youtubeVideoLink", async (req, res) => {
   videoDetails.push({ tags: videoTags });
 
   res.send(videoDetails);
+});
+
+app.post("/youtubeVideoThumbnailDownload", (req, res) => {
+  const image = req.body.image;
+  console.log(image);
+
+  options = {
+    url: image,
+    dest: "/path/to/dest/photo.jpg", // will be saved to /path/to/dest/photo.jpg
+  };
+
+  download
+    .image(options)
+    .then(({ filename }) => {
+      console.log("Saved to", filename); // saved to /path/to/dest/photo.jpg
+    })
+    .catch((err) => console.error(err));
 });
 
 app.listen(port, () => {
